5f1cd3222637b6abc8d0116f04cab5af6d824938
juliagoda <juliagoda.de@gmail.com>
2017-08-29 15:44:55 +0200

    Improved comments in GivenFind

--- src/GivenFind.hs
+++ src/GivenFind.hs
@@ -9,6 +9,28 @@ Written by Jagoda "juliagoda" Górska,  juliagoda.de@gmail.com
 Please start with the introduction at "GivenFind#intro".
 -}
 
+
+{- $intro
+ #intro#
+Welcome to GivenFind.
+
+This module provides search functions over texts in two formats - String
+or Text from Data.Text module. It is designed to get questions, commands
+and data with symbols and units from exercise of different branches. Most of the functions
+gets only one parameter - your exercise in String or Text format. If you think, that a list
+of units and symbols are not sufficient, you can use functions that take two
+parameters - your text and searched symbol or even list of symbols. 
+
+ If there are duplicates of names, I suggest using for example:
+
+>import qualified GivenFind.Physics as P
+>import qualified GivenFind.Geography as G
+>import qualified GivenFind.Chemistry as Ch
+...
+
+-}
+
+
 module GivenFind
 ( Symbols(..) 
 , WhereData(..)
@@ -33,24 +55,6 @@ import Control.Applicative
 import Data.Maybe
 import Data.List
 import Data.Char
-
-{- $intro
- #intro#
-Welcome to GivenFind.
-
-This module provides search functions over texts in two formats - String
-or Text from Data.Text module. It is designed to get questions, commands
-and data with symbols and units from exercise of different branches. Most of the functions
-gets only one parameter - your exercise in String or Text format. If you think, that a list
-of units and symbols are not sufficient, you can use functions that take two
-parameters - your text and searched symbol or even list of symbols. 
-
- If there are duplicates of names, I suggest using:
-
->import qualified GivenFind.Physics as P
->import qualified GivenFind.Base as G
-
--}
 
 
 data Symbols a = Symbol a| None deriving (Show, Eq, Ord, Read)
